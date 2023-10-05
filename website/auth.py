0c2b53b7bb05eb44f509415b5d8f1cfff190d138
Dominic Cummings <dominiccummings95@gmail.com>
2023-10-05 13:06:46 -0500

    Base template created

--- website/auth.py
+++ website/auth.py
@@ -1,3 +1,17 @@
 from flask import Blueprint
+
 #setup blueprints for all the urls 
 auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+  return "<p>Login</p>"
+
+@auth.route('/logout')
+def logout():
+  return "<p>Logout</p>"
+
+@auth.route('/sign-up')
+def sign_up():
+  return "<p>Sign Up</p>"
+
